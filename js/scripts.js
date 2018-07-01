543f21bc8d80ee0eead2c48b87e8ba341982a87c
arnold keto <arnoldjohn20@gmail.com>
2018-07-01 22:34:30 +0300

    Add js details

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,66 @@
-//business logic
-var index = []
-for (var i = 1; i <= 100; i++) {
-    index.push(i)
+// //business logic
+// function config (){
+//
+//   for (var h=1; h<=100; h++){
+//   if(userInput%3===0){
+//     // $("#reply").append("<li>ping</li>")||$("#reply").append(h)
+//      $("#reply").append( "<li>ping</li>")
+//   } if (userInput%5===0) {
+//     // $("#reply").append("<li>pong</li>")||$("#reply").append(h)
+//     $("#reply").append( "<li>pong</li>")
+//   } if (userInput%3&&5===0) {
+//     // $("#reply").append("<li>pingpong</li>")||$("#reply").append(h)
+//     $("#reply").append( "<li>pingpong</li>")
+//   // } else {
+//   //   // $("#reply").text("not a value")
+//   //   $("#reply").append(output += "ping")
+//   // }
+//     }
+//     $("#reply").text(h)
+//   }
+//
+// }
+//
+//
+//
+// //UI logic
+// $(document).ready(function() {
+//   $("#replies-form").submit(function(event){
+//     event.preventDefault();
+//     var userInput = parseInt($("input#replies-Input").val());
+//       // $("replies-Input").val("");
+//     // var replies = $("replies-Input").val();
+//         config(userInput);
+//
+//
+// })
+
+// })
+//back-end logic
+function config(h){
+	//takes in an interger and integer as an argument and returns the below logical states
+	if (h%3===0){
+		return "ping"
+		}
+	else if(h%5===0){
+		return "pong"
+		}
+	else if(h%15===0){
+		return "pingpong"
+		}
+	else{
+		return h
+		}
 }
-var div3 = []
-for (var y = 3; y <= 100; y+=3){
-   div3.push(y)
-}
 
-var div5 = []
-for (var y = 5; y <= 100; y+=5){
-   div5.push(y)
-}
-
-var div15 = []
-for (var y = 15; y <= 100; y+=15){
-   div15.push(y)
-}
-
-
-
-
-//UI logic
-$(document).ready(function){
-$("#replies-form").submit(function(event){
-    event.preventDefault()
-    $("#replies-Input")
-
-})
-}
+//user interface
+$(document).ready(function(){
+	$("#replies-form").submit(function(event){
+		event.preventDefault();
+		$("#reply").empty();
+		var userInput=parseInt($("input#replies-Input").val());
+		for(var h=1; h<=userInput; h+=1){
+			$("#reply").append("<li>"+pingPong(h)+"</li>");
+		}
+	});
+});
